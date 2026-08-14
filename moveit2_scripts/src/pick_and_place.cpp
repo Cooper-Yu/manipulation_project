a5528f2a8e07524860a837003b58444361493d03
Yu Ge <liuzi9240@gmail.com>
2026-08-15 00:27:30 +0800

    Add Cartesian collision diagnostic probe

--- moveit2_scripts/src/pick_and_place.cpp
+++ moveit2_scripts/src/pick_and_place.cpp
@@ -135,10 +135,20 @@ int main(int argc, char * argv[])
       (approach_fraction >= minimum_cartesian_fraction);
 
     if (!approach_path_success) {
+      moveit_msgs::msg::RobotTrajectory approach_no_collision_probe;
+      const double approach_no_collision_fraction =
+        move_group.computeCartesianPath(
+          approach_waypoints,
+          approach_eef_step,
+          approach_jump_threshold,
+          approach_no_collision_probe,
+          false);
+
       RCLCPP_ERROR(
         node->get_logger(),
-        "APPROACH_CARTESIAN_PATH FAIL: fraction=%.3f; execution was not attempted.",
-        approach_fraction);
+        "APPROACH_CARTESIAN_PATH FAIL: collision_check_fraction=%.3f, no_collision_probe_fraction=%.3f; neither trajectory was executed.",
+        approach_fraction,
+        approach_no_collision_fraction);
     } else {
       RCLCPP_INFO(
         node->get_logger(),
