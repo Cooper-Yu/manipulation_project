708fd3d046a583e35436a1535946be17aad53088
Yu Ge <liuzi9240@gmail.com>
2026-09-06 23:11:59 +0800

    Add perception launch and RViz configuration

---
+++ object_detection/launch/object_detection.launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    rviz_config = os.path.join(get_package_share_directory("object_detection"), "rviz", "object_detection.rviz")
+    return LaunchDescription([
+        Node(package="object_detection", executable="object_detection", name="object_detection_node", output="screen"),
+        Node(package="rviz2", executable="rviz2", name="rviz2", output="screen", arguments=["-d", rviz_config]),
+    ])
